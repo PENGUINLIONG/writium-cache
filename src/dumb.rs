769e452c2e5fa3d7ae686787b4cc8884c3bcf528
PENGUINLIONG <admin@penguinliong.moe>
2017-12-26 19:26:47 +0800

    Add dumb cache source

---
+++ src/dumb.rs
@@ -0,0 +1,18 @@
+use std::marker::PhantomData;
+use writium_framework::prelude::*;
+use cache::CacheSource;
+
+const ERR_DUMB: &'static str = "Dumb cache is used, nothing is extracted.";
+
+pub struct DumbCacheSource<T: 'static + Send + Sync>(PhantomData<T>);
+impl<T: 'static + Send + Sync> DumbCacheSource<T> {
+    pub fn new() -> DumbCacheSource<T> {
+        DumbCacheSource(PhantomData)
+    }
+}
+impl<T: 'static + Send + Sync> CacheSource for DumbCacheSource<T> {
+    type Value = T;
+    fn load(&self, _id: &str, _create: bool) -> Result<T> {
+        Err(Error::internal(ERR_DUMB))
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,7 @@ extern crate log;
 extern crate writium_framework;
 
 mod cache;
+mod dumb;
+
 pub use cache::{Cache, CacheSource};
+pub use dumb::DumbCacheSource;
